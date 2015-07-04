8deb91353c8349b0db15eebfd02c765cc2014ea5
FranciscoME <francisco.elguezabal@gmail.com>
2015-07-04 11:23:39 -0500

    Functions instead instructions in .R file

--- meXBTRClient.R
+++ meXBTRClient.R
@@ -1,102 +1,90 @@
-# -- ----------------------------------------------------------------------------------- #
-# -- meXBT API CONNECTOR --------------------------------------------------------------- #
-# -- License: GNU License V3 ----------------------------------------------------------- #
-# -- ----------------------------------------------------------------------------------- #
+# -- -------------------------------------------------------------------------------------------- #
+# -- meXBT API CONNECTOR ------------------------------------------------------------------------ #
+# -- License: GNU License V3 -------------------------------------------------------------------- #
+# -- -------------------------------------------------------------------------------------------- #
 
-# -- Install if required or Load necessary packages suppressing messages at console ---- #
+# -- Install if required or Load necessary packages suppressing messages at console ------------- #
 
-if (!require(base)) install.packages('base', quiet = TRUE)
-suppressMessages(library (base))       # basic R functions
-if (!require(httr)) install.packages('httr', quiet = TRUE)
-suppressMessages(library (httr))       # url reader
-if (!require(jsonlite)) install.packages('jsonlite', quiet = TRUE)
-suppressMessages(library (jsonlite))   # JSON parser
-if (!require(lubridate)) install.packages('lubridate', quiet = TRUE)
-suppressMessages(library (lubridate))  # treatment and modification for dates
-if (!require(plyr)) install.packages('plyr', quiet = TRUE)
-suppressMessages(library (plyr))       # General data treatment
-if (!require(RCurl)) install.packages('RCurl', quiet = TRUE)
-suppressMessages(library (RCurl))      # leer url's
-if (!require(xts)) install.packages('xts', quiet = TRUE)
-suppressMessages(library (xts))        # Time series utilities
-if (!require(zoo)) install.packages('zoo', quiet = TRUE)
-suppressMessages(library (zoo))        # Time series utilities
+if (!require(base)) install.packages('base', quiet = TRUE)              # basic R functions
+suppressMessages(library (base))
+if (!require(httr)) install.packages('httr', quiet = TRUE)              # URL read functions
+suppressMessages(library (httr))
+if (!require(jsonlite)) install.packages('jsonlite', quiet = TRUE)      # JSON parser/converter
+suppressMessages(library (jsonlite))
+if (!require(lubridate)) install.packages('lubridate', quiet = TRUE)    # Date Objects Utilities
+suppressMessages(library (lubridate))
+if (!require(plyr)) install.packages('plyr', quiet = TRUE)              # General data treatment
+suppressMessages(library (plyr))
+if (!require(RCurl)) install.packages('RCurl', quiet = TRUE)            # Read URL's and get data
+suppressMessages(library (RCurl))
+if (!require(xts)) install.packages('xts', quiet = TRUE)                # Extesible Time Series
+suppressMessages(library (xts))
+if (!require(zoo)) install.packages('zoo', quiet = TRUE)                # Time series utilities
+suppressMessages(library (zoo))
 
-options("scipen"=100)       # No scientific Notation when big or small numbers displayed
+options("scipen"=100)        # No scientific Notation when too big or too small numbers displayed
 
-# ------------------------------------------------------------------------------------- #
-# -- Get Tick Historical Prices from meXBT PUBLIC API --------------------------------- #
-# ------------------------------------------------------------------------------------- #
+# ----------------------------------------------------------------------------------------------- #
+# -- Get Tick Historical Prices from meXBT PUBLIC API ------------------------------------------- #
+# ----------------------------------------------------------------------------------------------- #
 
-# "since" parameter is the tick number from which you wish to fetch data, 0 is from the
-# very begining of our data
+# "since" parameter is the tick/trade number from which you want to fetch data, 0 is from the
+# very begining of our data and that is "2014-05-12 21:16:34 CDT" for both Btc/Usd and and Btc/Mxn
 
-HmeXBTBtcUsd1 <- "https://data.mexbt.com/trades/btcusd?since=0"               # BTC/USD
-HmeXBTBtcUsd2 <- getURL(HmeXBTBtcUsd1,cainfo=system.file("CurlSSL",
+meXBTHistoricPrices <- function(BtcPair,TimeZonePar,InfoSince)
+{
+HmeXBTBtcUsd1a <- paste("https://data.mexbt.com/trades/",BtcPair,sep="")
+HmeXBTBtcUsd1b <- paste(HmeXBTBtcUsd1a,"?since=",sep="")
+HmeXBTBtcUsd1c <- paste(HmeXBTBtcUsd1b,InfoSince,sep="")
+HmeXBTBtcUsd2  <- getURL(HmeXBTBtcUsd1c,cainfo=system.file("CurlSSL",
                  "cacert.pem",package="RCurl"))
 HmeXBTBtcUsd3 <- data.frame(fromJSON(HmeXBTBtcUsd2))
 
 BtcUsd <- data.frame(HmeXBTBtcUsd3$tid,
-          as.POSIXct(as.numeric(as.character(HmeXBTBtcUsd3$date)),            # BTC/USD
-          origin = '1970-01-01', tz='America/Mexico_City'),                   # Date
-          HmeXBTBtcUsd3$price, HmeXBTBtcUsd3$amount)                          # Formated
-colnames(BtcUsd) <- c("TickerID","TimeStamp","Price","Amount")                # Posixct
+as.POSIXct(as.numeric(as.character(HmeXBTBtcUsd3$date)),
+origin = '1970-01-01', tz='America/Mexico_City'),
+HmeXBTBtcUsd3$price, HmeXBTBtcUsd3$amount)
+colnames(BtcUsd) <- c("TickerID","TimeStamp","Price","Amount")
+return(BtcUsd)
+}
 
-HmeXBTBtcMxn1 <- "https://data.mexbt.com/trades/btcmxn?since=12205"           # 12205 an 
-HmeXBTBtcMxn2 <- getURL(HmeXBTBtcMxn1,cainfo=system.file("CurlSSL",           # arbitrary
-                 "cacert.pem",package="RCurl"))                               # Example
-HmeXBTBtcMxn3 <- data.frame(fromJSON(HmeXBTBtcMxn2))
+# ----------------------------------------------------------------------------------------------- #
+# -- Get Current Price Ticker of BitCoin from meXBT PUBLIC API ---------------------------------- #
+# ----------------------------------------------------------------------------------------------- #
 
-BtcMxn <- data.frame(HmeXBTBtcMxn3$tid,
-          as.POSIXct(as.numeric(as.character(HmeXBTBtcMxn3$date)),            # BTC/MXN
-          origin = '1970-01-01', tz='America/Mexico_City'),                   # Date
-          HmeXBTBtcMxn3$price, HmeXBTBtcMxn3$amount)                          # Formated
-colnames(BtcMxn) <- c("TickerID","TimeStamp","Price","Amount")                # Posixct
-
-# ------------------------------------------------------------------------------------- #
-# -- Get Actual Ticker of BitCoin from meXBT PUBLIC API ------------------------------- #
-# ------------------------------------------------------------------------------------- #
-
-meXBTQuery1  <- "https://data.mexbt.com/ticker/btcmxn"            # HTTP Query Address    
+meXBTTicker <- function(BtcPair)
+{
+meXBTQuery1  <- paste("https://data.mexbt.com/ticker/",BtcPair,sep="")
 meXBTQuery1G <- getURL(meXBTQuery1,cainfo=system.file("CurlSSL",
                 "cacert.pem",package="RCurl"))
-meXBTBtcMxn  <- data.frame(fromJSON(meXBTQuery1G))
-TmeXBTBtcMxn <- data.frame(Sys.time(),meXBTBtcMxn$last,meXBTBtcMxn$bid,meXBTBtcMxn$ask)
-colnames(TmeXBTBtcMxn) <- c("Date","Value","Bid","Ask")
+TickermeXBT  <- data.frame(fromJSON(meXBTQuery1G))
+TickermeXBT  <- data.frame(Sys.time(),TickermeXBT$last,TickermeXBT$bid,TickermeXBT$ask,
+                TickermeXBT$askCount, TickermeXBT$bidCount)
+colnames(TickermeXBT) <- c("Date","Value","Bid","Ask","AskCount","BidCount")
+return(TickermeXBT)
+}
 
-meXBTQuery1  <- "https://data.mexbt.com/ticker/btcusd"            # HTTP Query Address
-meXBTQuery1G <- getURL(meXBTQuery1,cainfo=system.file("CurlSSL",
-                "cacert.pem",package="RCurl"))
-meXBTBtcUsd  <- data.frame(fromJSON(meXBTQuery1G))
-TmeXBTBtcUsd <- data.frame(Sys.time(),meXBTBtcUsd$last,meXBTBtcUsd$bid,meXBTBtcUsd$ask)
-colnames(TmeXBTBtcUsd) <- c("Date","Value","Bid","Ask")
+# ----------------------------------------------------------------------------------------------- #
+# -- Get Current Order Book from meXBT PUBLIC API ----------------------------------------------- #
+# ----------------------------------------------------------------------------------------------- #
 
-# ------------------------------------------------------------------------------------- #
-# -- Order Book from meXBT PUBLIC API ------------------------------------------------- #
-# ------------------------------------------------------------------------------------- #
-
-meXBTOBQuery  <- "https://data.mexbt.com/order-book/btcusd"       # HTTP Query Address
+meXBTOrderBook <- function(BtcPair)
+{
+meXBTts <- Sys.time()
+meXBTOBQuery  <- paste("https://data.mexbt.com/order-book/",BtcPair,sep="")
 meXBTOBQuery1 <- getURL(meXBTOBQuery,cainfo=system.file("CurlSSL",
                  "cacert.pem",package="RCurl"))
-
-meXBTOBBids   <- data.frame(fromJSON(meXBTOBQuery1)[1])           # JSON to Data Frame
-meXBTOBBids$Side      <- "Long(Bid)"
+meXBTOBBids <- data.frame(fromJSON(meXBTOBQuery1)[1])
+meXBTOBBids$Side <- "Long(Bid)"
+meXBTOBBids <- meXBTOBBids[-length(meXBTOBBids[,1]),]
 colnames(meXBTOBBids) <- c("Price","Amount","Side")
 
-meXBTOBAsks   <- data.frame(fromJSON(meXBTOBQuery1)[2])           # JSON to Data Frame
-meXBTOBAsks$Side      <- "Short(Ask)"
+meXBTOBAsks <- data.frame(fromJSON(meXBTOBQuery1)[2])
+meXBTOBAsks$Side <- "Short(Ask)"
 colnames(meXBTOBAsks) <- c("Price","Amount","Side")
-meXBTBtcUsdOB <- rbind(meXBTOBBids,meXBTOBAsks)                   # Join Bids/Ask orders
+meXBTBtcUsdOB <- rbind(meXBTOBBids,meXBTOBAsks)
 
-meXBTOBQuery  <- "https://data.mexbt.com/order-book/btcmxn"       # HTTP Query Address
-meXBTOBQuery1 <- getURL(meXBTOBQuery,cainfo=
-                 system.file("CurlSSL","cacert.pem",package="RCurl"))
-
-meXBTOBBids   <- data.frame(fromJSON(meXBTOBQuery1)[1])           # JSON to Data Frame
-meXBTOBBids$Side      <- "Long(Bid)"
-colnames(meXBTOBBids) <- c("Price","Amount","Side")
-
-meXBTOBAsks   <- data.frame(fromJSON(meXBTOBQuery1)[2])           # JSON to Data Frame
-meXBTOBAsks$Side      <- "Short(Ask)"
-colnames(meXBTOBAsks) <- c("Price","Amount","Side")
-meXBTBtcMxnOB <- rbind(meXBTOBBids,meXBTOBAsks)                   # Join Bids/Ask orders
+meXBTBtcUsdOB <- data.frame(meXBTts,meXBTBtcUsdOB[,])
+colnames(meXBTBtcUsdOB) <- c("TimeStamp","Price","Amount","Side")
+return(meXBTBtcUsdOB)
+}
